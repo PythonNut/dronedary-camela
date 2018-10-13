1a3e4e9075052b06dcd7faa52baca196f7b2bb5d
PythonNut <PythonNut@users.noreply.github.com>
2018-10-13 03:10:30 -0700

    Make control actually legit

--- control.py
+++ control.py
@@ -1,13 +1,32 @@
 import socket
+import time
+import operator
+from functools import reduce
 
-UDP_IP = "192.168.0.1"
-UDP_PORT = 50000
-#MESSAGE = b"\x66\x80\x80\x30\x80\x80\x80\x80"
-MESSAGE = bytearray([102, 128, 128, 255, 128, 128, 128, 128, 12, 243, 153])
+class DroneController(object):
+    UDP_IP = "192.168.0.1"
+    UDP_PORT = 50000
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("", self.UDP_PORT))
+    
+    def __del__(self):
+        self.sock.close()
 
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
+    def float_to_byte_unsigned(self, f):
+        return round(f * 255)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    def float_to_byte_signed(self, f):
+        return round(f * 127) + 128
+
+    def fly(self, t, x, y):
+        t_b = self.float_to_byte_unsigned(t)
+        x_b = self.float_to_byte_signed(x)
+        y_b = self.float_to_byte_signed(x)
+        
+        params = [x_b, y_b, t_b, 0x80, 0x80, 0x80, 0x80, 0x0e]
+        checksum = reduce(operator.xor, params)
+        command = [102, *params, checksum, 153]
+        message = bytearray(command)
+        self.sock.sendto(message, (self.UDP_IP, self.UDP_PORT))
 
